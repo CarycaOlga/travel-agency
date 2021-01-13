822ddb3486c7eed4932cef39a039238bc9c03203
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-01-13 19:54:06 +0100

    start exercise

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -10,11 +10,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
-// TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const CHANGE_TAG = createActionName('CHANGE_TAG');
+// DONE - add other action types
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const changeTag = payload => ({ payload, type: CHANGE_TAG });
+// DONE - add other action creators
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -23,6 +27,16 @@ export default function reducer(statePart = [], action = {}) {
       return {
         ...statePart,
         searchPhrase: action.payload,
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        //FINISH IT CARYCA
+      };
+    case CHANGE_TAG:
+      return {
+        ...statePart,
+        //FINISH IT CARYCA
       };
     // TODO - handle other action types
     default:
