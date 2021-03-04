ca339d522049de88aa9f5a80fb6fb476bb9a0d63
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-03-04 10:53:11 +0100

    add packages and sendOrder functionality

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -6,8 +6,47 @@ import {Row, Col} from 'react-flexbox-grid';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 //import {setOrderOption} from '../../../redux/orderRedux';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {settings} from '../../../data/settings';
 
-const OrderForm = ({optionsOrder, setOrderOption, tripCost}) => (
+const sendOrder = (options, tripCost, countryId, countryName, countryCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  if ((options.name == '') || (options.contact == '')) {
+    window.alert('Please fill name and contact form');
+  }else {
+    const payload = {
+      countryId,
+      countryName,
+      countryCode,
+      ...options,
+      totalCost,
+    };
+  
+
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+};
+
+const OrderForm = ({optionsOrder, setOrderOption, tripCost, countryName, countryCode, countryId}) => (
   <Row>
     {pricing.map(item => (
       <Col md={4} key={item.id}>
@@ -21,6 +60,7 @@ const OrderForm = ({optionsOrder, setOrderOption, tripCost}) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} orderOption={optionsOrder}/> 
     </Col>
+    <Button onClick={() => sendOrder(optionsOrder, tripCost, countryName, countryCode, countryId)}>Order Now!</Button>
   </Row>
 
 );
@@ -30,6 +70,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   optionsOrder: PropTypes.object,
   setOrderOption: PropTypes.func,
+  countryName: PropTypes.string,
+  countryId: PropTypes.string,
+  countryCode: PropTypes.string,
 };
 
 export default OrderForm;
