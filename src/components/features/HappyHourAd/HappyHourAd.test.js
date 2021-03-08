36ebe5547f3b3357ac446ac94c4b9bb16da93b02
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-03-08 12:18:31 +0100

    rename HappyHourAdd -> HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import HappyHourAd from './HappyHourAdd';
+import HappyHourAd from './HappyHourAd';
 
 const select = {
   title: '.title',
