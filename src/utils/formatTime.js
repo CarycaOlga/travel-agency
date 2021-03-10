dbbfa7a8791763e1795dac75bc69231296ccaf55
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-03-10 17:01:51 +0100

    finish formatTime function

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,14 @@
-export const formatTime = () => 'formatted time';
+export const pad0 = (num) => {
+  if(num > 9) return num;
+  else return `0${num}`;
+};
+
+export const formatTime = (time) => {
+  if(!time || typeof time !== 'number' || time < 0 ) return null;
+
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time - (hours * 3600)) / 60);
+  const seconds = time - (hours * 3600) - (minutes * 60); 
+
+  return `${pad0(hours)}:${pad0(minutes)}:${pad0(seconds)}`;
+};
